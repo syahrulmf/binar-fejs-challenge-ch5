5450a26fafecbcd27e9391de1cf9b66c243a5b9a
syahrulmf <50761419+syahrulmf@users.noreply.github.com>
2022-04-18 13:40:00 +0700

    setting & slicing

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
+import FormSearch from './components/Form/FormSearch';
+import Search from './pages/Search/Search';
+import Detail from './pages/Detail/Detail';
+import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
+import { Route, Routes } from 'react-router-dom';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Test</h1>
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<FormSearch />} />
+        <Route path='/search' element={<Search />} />
+        <Route path='/detail/:id' element={<Detail />} />
+        <Route path='*' element={<div className="text-center"><h1>Error: Halaman tidak tersedia</h1></div>} />
+      </Routes>
+      <Footer />
+    </>
   );
 }
 
